f483f1401a24c759a679da8f849ba32cdeada78e
jojo157 <carrigj@tcd.ie>
2021-06-16 20:04:24 +0000

    testing view

--- app.py
+++ app.py
@@ -17,7 +17,8 @@ mongo = PyMongo(app)
 def get_username():
     details = mongo.db.users.find()
     for detail in details:
-    return detail
+        username = detail.username
+    return username
 
 if __name__ == "__main__":
     app.run(host = os.environ.get("IP"), port = int(os.environ.get("PORT")), debug = True)
