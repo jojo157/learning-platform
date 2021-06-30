b831945d2313d201e6617c3c38d829fd4a7f0f7d
jojo157 <carrigj@tcd.ie>
2021-06-30 20:08:22 +0000

    added scrollintoview function to expand article to ensure user is presented with correct article and added offset so article is clearly visible. Add tooltip for icons

--- static/js/script.js
+++ static/js/script.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
     $('.materialboxed').materialbox();
     M.textareaAutoResize($('#body'));
     $('body').Aplus();
+    $('.tooltipped').tooltip();
 
   });
   
@@ -17,14 +18,14 @@ $(document).ready(function(){
     if ($(this).hasClass('active')){
       $('.content-container').removeClass('col s12 m8 offset-m2 l8 offset-l2 content-container').addClass('col s12 l4 m6 content-container'); 
       $(this).removeClass('active');
-      $(this).scrollIntoView();
+      
     }
   else {
        $(this).addClass('active');
        $('.content-container').removeClass('col s12 l4 m6 content-container').addClass('col s12 m8 offset-m2 l8 offset-l2 content-container');
-       $(this).scrollIntoView();
-      }  
-  });
+       $(this)[0].scrollIntoView(0, -40);
+  }
+      });
 
   
 
