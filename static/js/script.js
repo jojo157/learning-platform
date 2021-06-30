566d5484169bc33c526a3fe1c81339b323835729
jojo157 <carrigj@tcd.ie>
2021-06-30 18:57:35 +0000

    added seperate icon to increase the article size when clickde and keep the expand content seperate

--- static/js/script.js
+++ static/js/script.js
@@ -13,16 +13,17 @@ $(document).ready(function(){
 
   // code to zoom in on content when click and zoom out when clicked again
 
-  $('.content-container').click(function() {
+  $('i.fas.fa-expand-alt').click(function() {
     if ($(this).hasClass('active')){
-      
-      $('.content-container').removeClass('col s12 m8 offset-m2 l8 offset-l2 content-container active').addClass('col s12 l4 m6 content-container');
-  }
+      $('.content-container').removeClass('col s12 m8 offset-m2 l8 offset-l2 content-container').addClass('col s12 l4 m6 content-container'); 
+      $(this).removeClass('active');
+      $(this).scrollIntoView();
+    }
   else {
-
-       $('.content-container').removeClass('col s12 l4 m6 content-container').addClass('col s12 m8 offset-m2 l8 offset-l2 content-container active');
-  }
-    
+       $(this).addClass('active');
+       $('.content-container').removeClass('col s12 l4 m6 content-container').addClass('col s12 m8 offset-m2 l8 offset-l2 content-container');
+       $(this).scrollIntoView();
+      }  
   });
 
   
