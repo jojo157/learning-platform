7cb40a99f5587dc7af638f18f08592bac171e649
jojo157 <carrigj@tcd.ie>
2021-07-05 21:41:06 +0000

    issue still persists on mobile iOS, reversing return to orignal

--- app.py
+++ app.py
@@ -38,6 +38,7 @@ def before_request():
         code = 301
         return redirect(url, code=code)
 
+
 @app.route("/home")
 def home():
     if not session.get("user") is None:
@@ -53,9 +54,7 @@ def score_up(rated_article):
     current_score = document["rating_up"]
     new_score = current_score + 1
     mongo.db.content.update_one({"_id": ObjectId(rated_article)}, { "$set": {"rating_up": new_score} })
-    resp = make_response('', 204)
-    resp.headers['Content-Length'] = 0
-    return resp
+    return Response('', status=204)
     
 
 
@@ -65,9 +64,7 @@ def score_down(rated_article):
     current_score = document["rating_down"]
     new_score = current_score + 1
     mongo.db.content.update_one({"_id": ObjectId(rated_article)}, { "$set": {"rating_down": new_score} })
-    resp = make_response('', 204)
-    resp.headers['Content-Length'] = 0
-    return resp
+    return Response('', status=204)
 
 
 @app.route("/search", methods=["GET", "POST"])
@@ -292,9 +289,7 @@ def fav_content(content_id):
                 }
                 mongo.db.favourites.insert_one(favour)  
 
-                resp = make_response('', 204)
-                resp.headers['Content-Length'] = 0
-            return resp   
+                return Response('', status=204)  
         return render_template("register.html")
 
 @app.route("/delete_fav/<string:fav_title>", methods=["GET", "POST"])
