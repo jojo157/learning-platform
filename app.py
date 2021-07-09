95ba3f874cc590a56d5705fe95b13f0c0523f260
jojo157 <carrigj@tcd.ie>
2021-07-09 07:46:30 +0000

    added form validation to user settings page and to add site content and notes

--- app.py
+++ app.py
@@ -171,22 +171,18 @@ def login():
     """
     if request.method == "POST":
         # check if username exists in db
-        existing_user = mongo.db.users.find_one(
-            {"username": request.form.get("username").lower()})
+        existing_user = mongo.db.users.find_one({"username": request.form.get("username").lower()})
 
         if existing_user:
             # ensure hashed password matches user input
-            if check_password_hash(
-                    existing_user["password"], request.form.get("password")):
-                        session["user"] = request.form.get("username").lower()
-                        session["access"] = existing_user["access_level"]
-                        return redirect(url_for("home"))
-                            
+            if check_password_hash(existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                session["access"] = existing_user["access_level"]
+                return redirect(url_for("home"))                
             else:
                 # invalid password match
                 flash("Incorrect Username and/or Password", "error")
                 return redirect(url_for("login"))
-
         else:
             # username doesn't exist
             flash("Incorrect Username and/or Password", "error")
