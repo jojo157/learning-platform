1ed09f6b3be9d8b2013fd97a2096e2501c8f1a0d
jojo157 <carrigj@tcd.ie>
2021-07-11 20:25:49 +0000

    added further styling for different devices to improve layout

--- static/js/script.js
+++ static/js/script.js
@@ -7,23 +7,7 @@ $(document).ready(function(){
     $('.modal').modal();
   });
   
- 
-//Library (home) page interactions
-  // code to zoom in on content when click and zoom out when clicked again
 
-  $('i.fas.fa-expand-alt').click(function() {
-    if ($(this).hasClass('active')){
-      $('.content-container').removeClass('col s12 m8 offset-m2 l8 offset-l2 content-container').addClass('col s12 l4 m6 content-container'); 
-      $(this).removeClass('active');
-      $(this)[0].scrollIntoView(0, -40);
-      
-    }
-  else {
-       $(this).addClass('active');
-       $('.content-container').removeClass('col s12 l4 m6 content-container').addClass('col s12 m8 offset-m2 l8 offset-l2 content-container');
-       $(this)[0].scrollIntoView(0, -40);
-  }
-      });
 
  // code to send click data on content to python function to add to database
 //thumbs up - adds 1 to current score in databse and refreshes  the dom and keeps location
