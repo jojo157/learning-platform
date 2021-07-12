fa3383808c79d519c560b60172e4abcf660f101c
jojo157 <carrigj@tcd.ie>
2021-07-12 19:36:53 +0000

    added missing semi colons on textarea.js file

--- static/js/textarea.js
+++ static/js/textarea.js
@@ -1,8 +1,11 @@
+/*jshint esversion: 6 */
+
+
 // Text area validation when nothing but whitespace
 //credit to Sean in Tutoring
 const textAreas = document.getElementsByTagName('textarea');
 [...textAreas].forEach(ta => {
 ta.onchange = () => {
-ta.value = ta.value.trimStart()
-}
-})
+ta.value = ta.value.trimStart();
+};
+});
